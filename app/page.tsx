09e52974be363625fe7822497c729e494b6db093
Minsup Lee <m1nsuppp@gmail.com>
2023-04-28 22:11:53 +0900

    style: no mean, no feature

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,12 @@
+import { IconAddReaction } from '@/assets/icons';
+
 export default function Home() {
-  const arrayLength = 10;
-
-  const foo: number[] = Array(arrayLength).fill(0);
-
   return (
     <main className="min-h-screen flex justify-center items-center">
-      {foo.map((_, index) => (
-        <span key={index} className="mx-1 hover:text-red-400">
-          {++index}
-        </span>
+      {'m1nsuppp'.split('').map((_, index) => (
+        <code key={index}>
+          <IconAddReaction width={index * 12} height={index * 12} />
+        </code>
       ))}
     </main>
   );
