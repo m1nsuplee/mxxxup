9eb6b43778ca36966ee63d91f0d2d51831046c9f
Minsup Lee <m1nsuppp@gmail.com>
2023-05-09 21:56:34 +0900

    feat: 모달 바깥 영역 클릭시 unmount

--- components/modal.tsx
+++ components/modal.tsx
@@ -1,26 +1,43 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { MouseEvent, ReactNode, useEffect, useState } from 'react';
 import { createPortal } from 'react-dom';
 
-type ModalProps = { children: React.ReactNode };
+type ModalProps = {
+  isOpen: boolean;
+  onClose?: () => void;
+  children: ReactNode;
+};
 
-const Modal: React.FC<ModalProps> = ({ children }) => {
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose, children }) => {
   const [modalHTMLElement, setModalHTMLElement] = useState<HTMLElement | null>(
     null
   );
-
-  useEffect(() => {
-    setModalHTMLElement(document.getElementById('modal'));
-  }, []);
 
   if (!modalHTMLElement) {
     return null;
   }
 
+  useEffect(() => {
+    setModalHTMLElement(document.getElementById('modal'));
+  }, []);
+
+  const handleModalClose = () => onClose && onClose();
+
+  const stopPropagteModalChildEvent = (e: MouseEvent) => {
+    e.stopPropagation();
+  };
+
   return createPortal(
-    <div className="z-50 absolute top-0 left-0 overflow-hidden w-screen h-screen flex justify-center items-center">
-      {children}
+    <div
+      className={`
+        z-50 absolute top-0 left-0 overflow-hidden 
+        w-screen h-screen justify-center items-center
+        ${isOpen ? 'flex' : 'hidden'}
+    `}
+      onClick={handleModalClose}
+    >
+      <div onClick={stopPropagteModalChildEvent}>{children}</div>
     </div>,
     modalHTMLElement
   );
