f7a78c53273b899f16950c5fd1c25c240ad01826
Minsup Lee <m1nsuppp@gmail.com>
2023-05-16 02:01:37 +0900

    refactor: 디렉토리 구조 변경

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-import { Database } from '@/types/supabase';
+import { Database } from '@/types';
 
 export const supabase = createClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL || '',
