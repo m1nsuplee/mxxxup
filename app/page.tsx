9183a188931fc4a9a2c721b4c5b15822f2b09763
Minsup Lee <m1nsuppp@gmail.com>
2023-05-16 01:57:42 +0900

    feat: `supabase`에 저장된 data fetching

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
-export default function Home() {
-  return <main>null.</main>;
+import { supabase } from '@/lib/supabaseClient';
+
+export async function fetchCountries() {
+  const { data: countries } = await supabase.from('countries').select();
+
+  return countries;
+}
+
+export default async function Home() {
+  const countries = (await fetchCountries()) || [];
+
+  return (
+    <main>
+      {countries.map((country) => (
+        <p key={country.id}>{country.name}</p>
+      ))}
+    </main>
+  );
 }
