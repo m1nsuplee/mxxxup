92692beceeec42c804f369ddde8e70184f5b8823
Minsup Lee <m1nsuppp@gmail.com>
2023-05-16 01:19:42 +0900

    feat: 컬러 정의

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#005EFE',
+        'primary-hover': '#1A6EDD',
+        'border-gray': '#C6C6C6',
+        error: '#E3001E',
+        gray: '#7A7A7A',
+        disabled: '#EBEBE4',
+      },
+    },
   },
   plugins: [],
 };
