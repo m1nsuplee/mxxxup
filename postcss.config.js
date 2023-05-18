bb25b1fd70fcad5f550247bd1dfc790c08c490fd
Minsup Lee <m1nsuppp@gmail.com>
2023-05-19 03:19:23 +0900

    feat: upgrade to Next `13.4`

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {
-      grid: true,
-      flexbox: true,
-      overrideBrowserslist: ['last 2 versions', 'not dead'],
-    },
+    autoprefixer: {},
   },
 };
