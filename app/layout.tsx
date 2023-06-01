768cf5d434bc66f8b664e06d813164a16dfad5b0
Minsup Lee <m1nsuppp@gmail.com>
2023-06-02 01:06:31 +0900

    feat: 폰트 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,15 @@ import '@/styles/globals.css';
 import type { Metadata } from 'next';
 import ClientLayout from './client-layout';
 import { cn } from '@/lib';
+import localFont from 'next/font/local';
 
 type RootLayoutProps = {
   children: React.ReactNode;
 };
+
+const pretendardFont = localFont({
+  src: '../public/pretendard-variable.woff2',
+});
 
 export const metadata: Metadata = {
   title: siteConfig.title,
@@ -19,7 +24,8 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <body
         className={cn(
           'min-h-screen bg-primary text-white',
-          'flex justify-center items-start'
+          'flex justify-center items-start',
+          pretendardFont.className
         )}
       >
         <ClientLayout>{children}</ClientLayout>
