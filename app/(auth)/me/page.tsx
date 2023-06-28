ee7df9fe54313304c60b2ab41d65e9ce185385b0
Minsup Lee <m1nsuppp@gmail.com>
2023-06-29 01:20:19 +0900

    feat: 인증 실패 UI

--- app/(auth)/me/page.tsx
+++ app/(auth)/me/page.tsx
@@ -1,23 +1,11 @@
-import { getCurrentUser, Path } from '@/lib';
-import { Profile } from '@/components/auth';
-import { Button } from '@/components/common';
-import Link from 'next/link';
+import { getCurrentUser } from '@/lib';
+import { AuthenticationFail, Profile } from '@/components/auth';
 
 export default async function MyPage() {
   const currentUser = await getCurrentUser();
 
   if (!currentUser) {
-    return (
-      <div className="max-w-3xl w-full h-screen flex flex-col justify-center items-center gap-y-10">
-        <h1 className="text-xl">{'사용자 정보가 존재하지 않습니다.'}</h1>
-        <Link
-          href={Path.login}
-          className="w-60 h-14"
-        >
-          <Button type="button">{'로그인'}</Button>
-        </Link>
-      </div>
-    );
+    return <AuthenticationFail />;
   }
 
   return (
