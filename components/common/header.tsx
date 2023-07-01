70c0f6cc9cbf13d8e6675d38bf15791b2fe91249
Minsup Lee <m1nsuppp@gmail.com>
2023-07-01 16:08:39 +0900

    feat: 회원가입 버튼 추가

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -5,10 +5,19 @@ import Button from './button';
 import { Path } from '@/lib';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
+import { Montserrat } from 'next/font/google';
+import clsx from 'clsx';
 
 type HeaderProps = {
   user: User | undefined;
 };
+
+const logoFont = Montserrat({
+  preload: true,
+  style: 'normal',
+  weight: '200',
+  subsets: ['latin'],
+});
 
 const isAuthPage = (page: string | null) => {
   if (page) {
@@ -24,12 +33,17 @@ export default function Header({ user }: HeaderProps) {
   const currentPage = usePathname();
 
   return (
-    <header className="w-full flex justify-between items-center h-10">
-      <Link href={Path.home}>{'skrrt'}</Link>
+    <header className="w-full h-10 flex justify-between items-center">
+      <Link
+        href={Path.home}
+        className={clsx(logoFont.className, 'text-2xl')}
+      >
+        {'idkyet'}
+      </Link>
       {isAuthPage(currentPage) ? null : (
         <Link
           href={user ? Path.auth.mypage : Path.auth.login}
-          className={user ? 'w-10 h-10' : 'w-16 h-10'}
+          className={user ? 'w-10 h-10' : 'h-10 w-40'}
         >
           {user ? (
             <Image
@@ -40,13 +54,22 @@ export default function Header({ user }: HeaderProps) {
               className="w-full h-full rounded-full"
             />
           ) : (
-            <Button
-              type="button"
-              variant="grayscale"
-              className="text-sm text-gray-400 hover:text-white"
-            >
-              {'로그인'}
-            </Button>
+            <div className="flex gap-x-4">
+              <Button
+                type="button"
+                variant="grayscale"
+                className="text-sm text-gray-400 hover:text-white"
+              >
+                {'로그인'}
+              </Button>
+              <Button
+                type="button"
+                variant="primary"
+                className="text-sm text-gray-100 hover:text-white"
+              >
+                {'회원가입'}
+              </Button>
+            </div>
           )}
         </Link>
       )}
